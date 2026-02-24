d22216dc2522b2af225e4a1f6ee3ff445f718fa3
Kingsley Onwus <kingslex1995@gmail.com>
2026-02-24 10:41:12 +0100

    First Commit - Stock Ticker

---
+++ Stock Ticker/fakeStockAPI.js
@@ -0,0 +1,8 @@
+function getStockData() {
+    return {
+        name: 'QtechAI',
+        sym: 'QTA',
+        price: /* returns a random number between 0 and 3 to two decimal places*/,
+        time: /* returns a timestamp in the format: hh/mm/ss */
+    }
+}
