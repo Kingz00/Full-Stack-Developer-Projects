3d01f73ca54fa0d511c6d95920b375843ec02a80
Kingsley Onwus <kingslex1995@gmail.com>
2026-02-26 12:26:07 +0100

    Real Estate Super Challenge - First Commit

---
+++ Real Estate/properties/placeholderPropertyObj.js
@@ -0,0 +1,7 @@
+const placeholderPropertyObj = {
+    propertyLocation: 'Anywhere, London',
+    priceGBP: 100000,
+    roomsM2: [16, 12, 6, 11, 5],
+    comment: 'This is a placeholder property object.',
+    image: ''
+}
---
+++ Real Estate/properties/propertyForSaleArr.js
@@ -0,0 +1,73 @@
+const propertyForSaleArr = [
+    {
+        propertyLocation: 'Notting Hill, London',
+        priceGBP: 1200000,
+        roomsM2: [12, 16, 12, 8, 5],
+        comment: 'Charming Victorian property in a prime location!',
+        image: ''
+    },
+    {
+        propertyLocation: 'Chelsea, London',
+        priceGBP: 1500000,
+        roomsM2: [16, 20, 16, 12, 8],
+        comment: 'Luxury apartment in the heart of Chelsea!',
+        image: ''
+    },
+    {
+        propertyLocation: 'Camden, London',
+        priceGBP: 750000,
+        roomsM2: [10, 14, 10, 6, 4],
+        comment: 'Modern flat in a vibrant area!',
+        image: ''
+    },
+    {
+        propertyLocation: 'Greenwich, London',
+        priceGBP: 650000,
+        roomsM2: [12, 16, 12, 8, 5],
+        comment: 'Spacious property with stunning river views!',
+        image: ''
+    },
+    {
+        propertyLocation: 'Islington, London',
+        priceGBP: 800000,
+        roomsM2: [14, 18, 14, 10, 6],
+        comment: 'Beautifully renovated property in a trendy area!',
+        image: ''
+    },
+    {
+        propertyLocation: 'Hampstead, London',
+        priceGBP: 1100000,
+        roomsM2: [16, 20, 16, 12, 8],
+        comment: 'Elegant property in a prestigious neighborhood!',
+        image: ''
+    },
+    {
+        propertyLocation: 'Shoreditch, London',
+        priceGBP: 700000,
+        roomsM2: [10, 14, 10, 6, 4],
+        comment: 'Stylish apartment in a vibrant area!',
+        image: ''
+    },
+    {
+        propertyLocation: 'South Bank, London',
+        priceGBP: 900000,
+        roomsM2: [12, 16, 12, 8, 5],
+        comment: 'Modern property with stunning river views!',
+        image: ''
+    },
+    {
+        propertyLocation: 'Mayfair, London',
+        priceGBP: 2000000,
+        roomsM2: [20, 24, 20, 16, 12],
+        comment: 'Luxurious property in the heart of Mayfair!',
+        image: ''
+    },
+    {
+        propertyLocation: 'Clapham, London',
+        priceGBP: 600000,
+        roomsM2: [10, 14, 10, 6, 4],
+        comment: 'Charming property in a vibrant area!',
+        image: ''
+    }
+
+]
