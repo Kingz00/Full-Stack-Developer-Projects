0a72910c98a75a6ba53e6369e97dd6cf0c7a683c
Kingsley Onwus <kingslex1995@gmail.com>
2026-03-25 12:09:38 +0100

    Travel Journal First Commit

---
+++ Travel Journal/components/Header.jsx
@@ -0,0 +1,8 @@
+export default function Header() {
+    return (
+        <header>
+            <img src="../images/globe.png" className="header-logo" alt="Globe Icon" />
+            <span>my travel journal.</span>
+        </header>
+    )
+}
