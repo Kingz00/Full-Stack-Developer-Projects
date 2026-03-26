f737968b976eb7e9c298b81e6b8fa4b2612e5d6c
Kingsley Onwus <kingslex1995@gmail.com>
2026-03-26 12:43:26 +0100

    Adding more data entries

--- Travel Journal/App.jsx
+++ Travel Journal/App.jsx
@@ -1,11 +1,29 @@
 import Header from './components/Header.jsx'
 import Entry from './components/Entry.jsx'
+import data from './data.js'
 
-export default function App() {
+const App = () => {
+
+    const entryElements = data.map((entryObj) => {
+        const { id, img, title, country, googleMapsLink, dates, text } = entryObj
+        return (
+            <Entry
+                key={id}
+                img={img}
+                title={title}
+                country={country}
+                googleMapsLink={googleMapsLink}
+                dates={dates}
+                text={text}
+            />)
+    })
+
     return (
         <>
             <Header />
-            <Entry />
+            {entryElements}
         </>
     )
 }
+
+export default App
