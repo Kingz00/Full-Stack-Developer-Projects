5462038df0168182f5897e30afd697f832b61d96
Kingsley Onwus <kingslex1995@gmail.com>
2026-03-27 15:12:20 +0100

    Added a Form to the main componnent

--- Chef Claude/components/Main.jsx
+++ Chef Claude/components/Main.jsx
@@ -1,14 +1,32 @@
 const Main = () => {
+
+    let ingredients = ['Chicken', 'Oregano', 'Tomatoes']
+    const ingredientElements = ingredients.map((ingredient, index) => {
+        return <li key={index}>{ingredient}</li>
+    })
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        const formData = new FormData(e.currentTarget)
+        const newIngredient = formData.get("ingredient")
+        ingredients.push(newIngredient)
+        console.log(ingredients)
+    }
+
     return (
         <main>
-            <form className="add-ingredient-form">
+            <form onSubmit={handleSubmit} className="add-ingredient-form">
                 <input
                     type="text"
                     placeholder="e.g. maggi"
                     aria-label="Add ingredient"
+                    name="ingredient"
                 />
                 <button>Add Ingredient</button>
             </form>
+            <ul>
+                {ingredientElements}
+            </ul>
         </main>
     )
 }
