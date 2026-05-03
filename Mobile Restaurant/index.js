84602c510e28c890fe00fcde012b34fcfd49ef84
Kingz00 <87059410+Kingz00@users.noreply.github.com>
2026-05-03 14:16:45 +0100

    Added the addItem function in index.js to add menuItems to the order section and get the total price

--- Mobile Restaurant/index.js
+++ Mobile Restaurant/index.js
@@ -1,10 +1,35 @@
 import { menuArray } from "./data";
 
 const menuContainer = document.querySelector("#menu-container")
+const orderContainer = document.querySelector("#order-container")
+
+
+const orders = []
+const orderEls = []
 
 document.addEventListener("click", (e) => {
-
+    if (e.target.dataset.menuItem) {
+        addItem(e.target.dataset.menuItem)
+    }
 })
+
+const addItem = (menuId) => {
+    const menuObj = menuArray.filter((obj) => { return Number(menuId) === obj.id })[0]
+    orders.push(menuObj)
+    orderEls.push(`
+        <div class="order-item-container">
+            <h2 class="order-item">${menuObj.name}</h2>
+            <button class="remove-btn">remove</button>
+            <p class="price">${menuObj.price}</p>
+        </div>
+        `)
+    document.getElementById("order-items-container").innerHTML = orderEls.join('')
+    const totalPrice = orders.reduce((acc, currentValue) => {
+        return acc + currentValue.price
+    }, 0)
+    document.getElementById("total-price").textContent = totalPrice
+    orderContainer.style.display = "block"
+}
 
 const getMenus = () => {
     const menus = menuArray.map((menu) => {
@@ -16,15 +41,15 @@ const getMenus = () => {
                     <p class="ingredients">${menu.ingredients.join(",")}</p>
                     <p class="price">${menu.price}</p>
                 </div>
-                <button id="add-item" class="add-item">+</button>
+                <button id="add-item" class="add-item" data-menu-item="${menu.id}">+</button>
             </div>
         `
     }).join('')
     return menus
 }
 
-const render = () => {
-    menuContainer.innerHTML = getMenus()
+const render = (domArray) => {
+    menuContainer.innerHTML = domArray
 }
 
-render()
+render(getMenus())
