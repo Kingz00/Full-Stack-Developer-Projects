d3205af2978a338300eed54e73510064844845a9
Kingz00 <87059410+Kingz00@users.noreply.github.com>
2026-05-11 08:11:33 +0100

    Fetched the color scheme from the Color API and rendered it to the DOM

--- Color Scheme Generator/index.js
+++ Color Scheme Generator/index.js
@@ -1,12 +1,28 @@
-const dropdown = document.getElementById('color-dropdown');
-const picker = document.getElementById('color-picker');
+const dropdown = document.getElementById('color-dropdown')
+const picker = document.getElementById('color-picker')
+const colorSchemeBtn = document.getElementById('color-scheme-btn')
+const colorSchemeSection = document.getElementById('color-scheme-section')
 
-// Update picker when dropdown changes
-dropdown.addEventListener('change', (e) => {
-    picker.value = e.target.value;
-});
 
-// Optional: Update dropdown when picker changes (bi-directional sync)
-picker.addEventListener('input', (e) => {
-    dropdown.value = e.target.value;
-});
+colorSchemeBtn.addEventListener("click", async () => {
+    try {
+        const res = await fetch(`https://www.thecolorapi.com/scheme?hex=${picker.value.slice(1)}&mode=${dropdown.value}&count=5`)
+        if (!res.ok) {
+            throw new Error("Something went wrong")
+        }
+        const data = await res.json()
+        console.log(data.colors)
+        const colorSchemeEls = data.colors.map((colorObj) => {
+            return `
+                <div class="scheme-container">
+                    <div class="color-scheme" style="background-color: ${colorObj.hex.value};"></div>
+                    <span class="scheme-hex">${colorObj.hex.value}</span>
+                </div>
+            `
+        }).join('')
+        colorSchemeSection.innerHTML = colorSchemeEls
+    }
+    catch (err) {
+        console.error(err)
+    }
+})
