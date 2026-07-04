0d51fdc7a748d3ae929e365356e91e8e3ae7d010
Kingsley Onwus <kingslex1995@gmail.com>
2026-07-04 15:18:58 +0100

    Added the Dice component and functionality to randomly generate 10 dice and render to the page

--- Tenzies/App.jsx
+++ Tenzies/App.jsx
@@ -1,7 +1,29 @@
+import Die from "./Die.jsx"
+import { useState } from "react"
+
 const App = () => {
+
+    const generateAllNewDice = () => {
+        return new Array(10).fill(0).map(() => Math.ceil(Math.random() * 6))
+    }
+
+    const [dice, setDice] = useState(generateAllNewDice())
+
+    const dieEls = dice.map(die => <Die value={die} />)
+
+
+    const rollDice = () => {
+        setDice(generateAllNewDice())
+    }
+
+
     return (
         <main>
+            <div className="container">
+                {dieEls}
+            </div>
 
+            <button className="roll-dice" onClick={rollDice}>Roll</button>
         </main>
     )
 }
