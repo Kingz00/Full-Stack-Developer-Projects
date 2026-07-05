38a8b024fbb477debe72ab0c120e9d19cf83745e
Kingsley Onwus <kingslex1995@gmail.com>
2026-07-05 12:33:16 +0100

    Added functionality for when the game is won, proper css styling and some other accessibility improvements

--- Tenzies/App.jsx
+++ Tenzies/App.jsx
@@ -1,29 +1,77 @@
 import Die from "./Die.jsx"
-import { useState } from "react"
+import { useState, useRef, useEffect } from "react"
+import { nanoid } from "nanoid"
+import ReactConfetti from "react-confetti"
 
 const App = () => {
 
     const generateAllNewDice = () => {
-        return new Array(10).fill(0).map(() => Math.ceil(Math.random() * 6))
+        const diceArray = new Array(10).fill(0).map(() => Math.ceil(Math.random() * 6))
+        return diceArray.map(die => (
+            {
+                id: nanoid(),
+                value: die,
+                isHeld: false
+            }
+        ))
     }
 
-    const [dice, setDice] = useState(generateAllNewDice())
+    const [dice, setDice] = useState(() => generateAllNewDice())
 
-    const dieEls = dice.map(die => <Die value={die} />)
+    const gameWon = dice.every(die => die.isHeld) && dice.every(die => die.value === dice[0].value)
+
+    const setFocus = useRef(null)
+
+    useEffect(() => {
+        if (gameWon) {
+            setFocus.current.focus()
+        }
+    }, [gameWon])
+
+    const hold = (id) => {
+        setDice(prevDice => {
+            return prevDice.map(dieObj => {
+                return dieObj.id === id ? { ...dieObj, isHeld: !dieObj.isHeld } : dieObj
+            })
+        })
+    }
+
+    const dieEls = dice.map(dieObj => (
+        <Die
+            key={dieObj.id}
+            value={dieObj.value}
+            isHeld={dieObj.isHeld}
+            hold={() => hold(dieObj.id)}
+        />
+    ))
 
 
     const rollDice = () => {
-        setDice(generateAllNewDice())
+        if (gameWon) {
+            setDice(generateAllNewDice())
+        } else {
+            setDice(prevDice => {
+                return prevDice.map(dieObj => {
+                    return dieObj.isHeld ? dieObj : { ...dieObj, value: Math.ceil(Math.random() * 6) }
+                })
+            })
+        }
     }
 
 
     return (
         <main>
+            {gameWon && <ReactConfetti width={window.innerWidth} height={window.innerHeight} />}
+            <div aria-live="polite" className="sr-only">
+                {gameWon && <p>Congratulations! You won! Press "New Game" to start again.</p>}
+            </div>
+            <h1 className="title">Tenzies</h1>
+            <p className="instructions">Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
             <div className="container">
                 {dieEls}
             </div>
 
-            <button className="roll-dice" onClick={rollDice}>Roll</button>
+            <button ref={setFocus} className="roll-dice" onClick={rollDice}>{gameWon ? "New Game" : "Roll"}</button>
         </main>
     )
 }
--- Tenzies/Die.jsx
+++ Tenzies/Die.jsx
@@ -1,6 +1,17 @@
-const Die = ({ value }) => {
+const Die = ({ value, isHeld, hold }) => {
+
+    const styles = {
+        backgroundColor: isHeld ? "#59E391" : "#FFFFFF"
+    }
+
     return (
-        <button>{value}</button>
+        <button
+            style={styles}
+            onClick={hold}
+            aria-pressed={isHeld}
+            aria-label={`Die with value ${value}, 
+            ${isHeld ? "held" : "not held"}`}
+        >{value}</button>
     )
 }
 
