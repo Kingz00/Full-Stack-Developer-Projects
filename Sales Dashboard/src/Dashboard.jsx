f2f401d309862a994a5b4fd877de3924e63eaf32
Kingsley Onwus <kingslex1995@gmail.com>
2026-07-22 15:54:42 +0100

    Sales Team Dashboard - First Commit

---
+++ Sales Dashboard/src/Dashboard.jsx
@@ -0,0 +1,103 @@
+import React from "react";
+import supabase from "./supabase-client";
+import { Chart } from 'react-charts';
+
+function Dashboard() {
+
+    // Run the commands below in Supabase SQL editor to allow postgreSQL aggregates:
+    // 	 ALTER ROLE authenticator SET pgrst.db_aggregates_enabled = 'true';
+    //   NOTIFY pgrst, 'reload config';
+
+    const [metrics, setMetrics] = React.useState([])
+
+    const fetchMetrics = async () => {
+
+        try {
+
+            const { data, error } = await supabase
+                .from('sales_deals')
+                .select(
+                    `
+                    name,
+                    total_sales:value.sum()
+                    `,
+                )
+
+            if (error) {
+                throw new Error(`${error.message}`)
+            }
+
+            console.log(data)
+            setMetrics(data)
+        } catch (err) {
+            console.log(`Error fetching sales data: ${err}`)
+        }
+
+    }
+
+    React.useEffect(() => {
+        fetchMetrics()
+    }, [])
+
+    const chartData = [
+        {
+            data: metrics.map((m) => ({
+                primary: m.name,
+                secondary: m.total_sales,
+            })),
+        },
+    ];
+
+    const primaryAxis = {
+        getValue: (d) => d.primary,
+        scaleType: 'band',
+        padding: 0.2,
+        position: 'bottom',
+    };
+
+    const secondaryAxes = [
+        {
+            getValue: (d) => d.secondary,
+            scaleType: 'linear',
+            min: 0,
+            max: y_max(),
+            padding: {
+                top: 20,
+                bottom: 40,
+            },
+        },
+    ];
+
+    function y_max() {
+        if (metrics.length > 0) {
+            const maxSum = Math.max(...metrics.map((m) => m.sum));
+            return maxSum + 2000;
+        }
+        return 5000;
+    }
+
+
+    return (
+        <div className="dashboard-wrapper">
+            <div className="chart-container">
+                <h2>Total Sales This Quarter ($)</h2>
+                <div style={{ flex: 1 }}>
+                    <Chart
+                        options={{
+                            data: chartData,
+                            primaryAxis,
+                            secondaryAxes,
+                            type: 'bar',
+                            defaultColors: ['#58d675'],
+                            tooltip: {
+                                show: false,
+                            },
+                        }}
+                    />
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Dashboard;
